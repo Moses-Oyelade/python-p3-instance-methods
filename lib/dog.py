e49872c33c5787ce75761a2ccbf4d2bf903dc84c
Moses-Oyelade <oyelademoses@gmail.com>
2023-10-05 00:30:10 +0100

    all test passed

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python3
 
 class Dog:
-    # Class body goes here
+    pass 
 
-    #Instance method definition
-    pass
+    # Class body goes here
+    def _init_(self, bark, sit):
+        self.bark=bark
+        self.sit=sit
+        
+        
+    def bark(self):
+        print("Woof!")
+    
+    def sit(self):
+        print("The dog is sitting.")
+        
+dog1 = Dog()
+snoopy = Dog()
+
+print(dog1.bark())
+print(dog1.sit())
+print(snoopy.bark())
--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
-
-    #Instance method definition
     pass
+    # Class body goes here
+    def _init_(self, talk, walk):
+        self.talk=talk
+        self.walk=walk
+        
+        
+    def talk(self):
+        print("Hello World!")
+        return "Hello World!"
+    
+    def walk(self):
+        print("The person is walking.")
+        return "The person is walking."
+
+    
+person1 = Person()
+# person2 = Person()
+
+print(person1.talk())
+print(person1.walk())
+# print(person2.talk())
+        
+
